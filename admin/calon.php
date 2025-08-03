1996f7901cb7d1cf4418e2a0d21a4d611237c065
Pefiye <143986148+Pefiye@users.noreply.github.com>
2025-08-03 16:13:13 +0700

    progress2

--- admin/calon.php
+++ admin/calon.php
@@ -53,7 +53,7 @@ $loggedIn = isset($_SESSION['username']);
             <img src="img/icons/personbox.svg" alt="">
             </div>
             <div>
-            <h3>Calon Peserta Didik <br> Terdaftar</h3>
+            <h3> Calon Peserta Didik <br> Terdaftar</h3>
             <p>109</p>
             </div>
         </div>
@@ -68,29 +68,32 @@ $loggedIn = isset($_SESSION['username']);
                 </div>
             </div>
             <div class="tablecontent">
-                <div class="top">
-                    <p>ID</p>
-                    <p>NIK</p>
-                    <p>Nama Lengkap</p>
-                    <p>Tanggal Lahir</p>
-                    <p>Jenis Kelamin</p>
-                    <p>Alamat Email</p>
-                    <p>Nomor Telepon</p>
-                    <p>Minat Jurusan</p>
-                </div>
-                <div class="bottom">
-                    <div class="row">
-                        <p>001</p>
-                        <p>917498172984</p>
-                        <p>Wildan Izhar Al Haqq</p>
-                        <p>9/11/2025</p>
-                        <p>Female</p>
-                        <p>wildan@gmail.com</p>
-                        <p>08123456789</p>
-                        <p>RPL, DKV, TKJ</p>
-                    </div>  
-                </div>
-            </div>
+    <div class="tablewrapper">
+        <div class="top">
+            <p>ID</p>
+            <p>NIK</p>
+            <p>Nama Lengkap</p>
+            <p>Tanggal Lahir</p>
+            <p>Jenis Kelamin</p>
+            <p>Alamat Email</p>
+            <p>Nomor Telepon</p>
+            <p>Minat Jurusan</p>
+        </div>
+        <div class="bottom">
+            <div class="row">
+                <p>001</p>
+                <p>917498172984</p>
+                <p>Wildan Izhar Al Haqq</p>
+                <p>9/11/2025</p>
+                <p>Female</p>
+                <p>wildan@gmail.com</p>
+                <p>08123456789</p>
+                <p>RPL, DKV, TKJ</p>
+            </div>  
+        </div>
+    </div>
+</div>
+
             <div class="tableclosing"></div>
         </div>
     </div>
@@ -106,7 +109,9 @@ function handleSidebar() {
 
     if (!isMobile) {
         sidebar.style.display = 'flex';
+        checkbox2.style.display = 'none';
         checkbox.checked = false;
+        checkbox2.checked = false;
     } else {
         checkbox2.style.display = 'flex';
         sidebar.style.display = checkbox.checked ? 'flex' : 'none';
--- admin/pesdik.php
+++ admin/pesdik.php
@@ -68,29 +68,32 @@ $loggedIn = isset($_SESSION['username']);
                 </div>
             </div>
             <div class="tablecontent">
-                <div class="top">
-                    <p>ID</p>
-                    <p>NIK</p>
-                    <p>Nama Lengkap</p>
-                    <p>Tanggal Lahir</p>
-                    <p>Jenis Kelamin</p>
-                    <p>Alamat Email</p>
-                    <p>Nomor Telepon</p>
-                    <p>Minat Jurusan</p>
-                </div>
-                <div class="bottom">
-                    <div class="row">
-                        <p>001</p>
-                        <p>917498172984</p>
-                        <p>Wildan Izhar Al Haqq</p>
-                        <p>9/11/2025</p>
-                        <p>Female</p>
-                        <p>wildan@gmail.com</p>
-                        <p>08123456789</p>
-                        <p>RPL, DKV, TKJ</p>
-                    </div>  
-                </div>
-            </div>
+    <div class="tablewrapper">
+        <div class="top">
+            <p>ID</p>
+            <p>NIK</p>
+            <p>Nama Lengkap</p>
+            <p>Tanggal Lahir</p>
+            <p>Jenis Kelamin</p>
+            <p>Alamat Email</p>
+            <p>Nomor Telepon</p>
+            <p>Minat Jurusan</p>
+        </div>
+        <div class="bottom">
+            <div class="row">
+                <p>001</p>
+                <p>917498172984</p>
+                <p>Wildan Izhar Al Haqq</p>
+                <p>9/11/2025</p>
+                <p>Female</p>
+                <p>wildan@gmail.com</p>
+                <p>08123456789</p>
+                <p>RPL, DKV, TKJ</p>
+            </div>  
+        </div>
+    </div>
+</div>
+
             <div class="tableclosing"></div>
         </div>
     </div>
