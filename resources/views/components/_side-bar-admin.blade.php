adfb87707d035e0613afab075d4a09aad49d4719
pefiye <pefiyeplayz@gmail.com>
2025-10-03 20:59:19 +0700

    ngerapihin admin

--- resources/views/components/_side-bar-admin.blade.php
+++ resources/views/components/_side-bar-admin.blade.php
@@ -4,11 +4,11 @@
             <div class="container">
                 <img src="{{ asset("images/logo yayasan tin.png") }}" alt="logo yayasan tin">
                 <a href="{{ route("admin.dashboard") }}">
-                    <div class="options" id="dashboard"><img src="{{ asset("images/fj40.png") }}"
+                    <div class="options" id="dashboard"><img src="{{ asset("icons/dashboard.svg") }}"
                             alt="Dashboard icon"><label>Dashboard</label></div>
                 </a>
                 <a href="{{ route("candidates.index") }}">
-                    <div class="options-s" id="candidates"><img src="{{ asset("images/fj40.png") }}" alt="Calon icon"><label>Calon
+                    <div class="options-s" id="candidates"><img src="{{ asset("icons/siswa.svg") }}" alt="Calon icon"><label>Calon
                             Pesdik</label></div>
                 </a>
                 <input type="checkbox" id="check2">
